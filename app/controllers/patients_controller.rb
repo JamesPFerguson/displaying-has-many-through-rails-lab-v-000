b6352373919736da7f8e0e0cb981574552a11dd3
James Ferguson <ncjamesferguson@gmail.com>
2018-03-29 00:56:12 +0000

    automatically backed up by learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,17 +1,12 @@
 class PatientsController < ApplicationController
 
   def show
-    abstract_set
     @patient = Patient.find(params[:id])
-    binding.pry
   end
 
   def index
   end
 
 
-  def abstract_set
-    name = self.class.to_s.split("Controller")[0].chop
-    binding.pry
-  end
+
 end
