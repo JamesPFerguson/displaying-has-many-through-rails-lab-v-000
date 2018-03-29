e182966e69d3c2451d7f15f4ed6e178f3fa95575
James Ferguson <ncjamesferguson@gmail.com>
2018-03-29 00:10:47 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,8 @@
 class AppointmentsController < ApplicationController
 
   def show
+    @appointment = Appointment.find(params[:id])
+    @patient = @appointment.try(:patient)
+    @doctor = @appointment.try(:doctor)
   end
 end
